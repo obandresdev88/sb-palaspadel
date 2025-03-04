6f84e14131f82d031fa8d52c222176f110e1aed6
obandresdev88 <obandresdev88@gmail.com>
2025-03-04 07:59:06 +0100

    Configuración y estructura inicial con las dependecias que necesitmos desde spring initializr, compila comentando dependencia a Hibernate

---
+++ src/main/java/com/palaspadel/sb_palaspadel/SbPalaspadelApplication.java
@@ -0,0 +1,13 @@
+package com.palaspadel.sb_palaspadel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbPalaspadelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbPalaspadelApplication.class, args);
+	}
+
+}
