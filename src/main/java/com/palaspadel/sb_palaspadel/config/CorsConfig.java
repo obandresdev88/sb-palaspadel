45a3aa67caf42b8c66cb70b1df8567ac827acfc8
obandresdev88 <obandresdev88@gmail.com>
2025-03-04 11:58:21 +0100

    [DEV-03_Create-Packages] Estructura de paquetes en el proyecto, configuración Cors inicial para que el front pueda atacar al back

---
+++ src/main/java/com/palaspadel/sb_palaspadel/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.palaspadel.sb_palaspadel.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+/**
+ * Clase que configura el CORS para permitir el acceso al backend desde el frontend
+ */
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://127.0.0.1:5500/") // URL del frontend
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
