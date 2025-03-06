28cce3873d37e81a80950e4dd53bd5a5a9598841
obandresdev88 <obandresdev88@gmail.com>
2025-03-06 06:55:56 +0100

    [DEV-09_Drop-Niv-Register-Logic-User] Modificamos persistencia, registro de usuario con rol y relacion rol-user

--- src/main/java/com/palaspadel/sb_palaspadel/controllers/UsuController.java
+++ src/main/java/com/palaspadel/sb_palaspadel/controllers/UsuController.java
@@ -1,11 +1,12 @@
 package com.palaspadel.sb_palaspadel.controllers;
 
 import com.palaspadel.sb_palaspadel.entities.Usu;
+import com.palaspadel.sb_palaspadel.repositories.UsuRepository;
 import com.palaspadel.sb_palaspadel.services.UsuService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,5 +27,21 @@ public class UsuController {
     @GetMapping
     public List<Usu> listarUsuarios() {
         return usuService.findAll();
+    }
+
+    /**
+     * Metodo para crear un usuario llamando al servicio o capa de negocio (impl)
+     *
+     * @param usu
+     * @return
+     */
+    @CrossOrigin(origins = "http://127.0.0.1:5500") // Para permitir peticiones desde cualquier origen
+    @PostMapping("/registro")
+    public ResponseEntity<Usu> crearUsuario( @RequestBody Usu usu) { // Se pone body porque no vendrá por path variable, se rellena en el formulario
+        Usu usuSaved  = usuService.save(usu);
+        if (usuSaved == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).body(usuSaved);
     }
 }
