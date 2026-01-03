d867269b1051289bd731054b20e31f95c45d687f
obandresdev88 <obandresdev88@gmail.com>
2026-01-03 23:59:03 +0100

    Pongo la anotación para asegurar consistencia en las operaciones de BD

--- src/main/java/com/palaspadel/sb_palaspadel/services/PalService.java
+++ src/main/java/com/palaspadel/sb_palaspadel/services/PalService.java
@@ -3,6 +3,7 @@ package com.palaspadel.sb_palaspadel.services;
 import com.palaspadel.sb_palaspadel.dto.PalaActualizarDto;
 import com.palaspadel.sb_palaspadel.dto.PalaCrearDto;
 import com.palaspadel.sb_palaspadel.dto.PalaResponseDto;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
@@ -20,11 +21,15 @@ public interface PalService {
 
     PalaResponseDto obtenerPalaPorId(Integer id);
 
+    @Transactional
     PalaResponseDto crearPala(PalaCrearDto dto, MultipartFile imagen);
 
+    @Transactional
     PalaResponseDto actualizarPala(Integer id, PalaActualizarDto dto, MultipartFile imagen);
 
+    @Transactional
     void eliminarPala(Integer id);
 
+    @Transactional
     int importarPalasDesdeExcel(MultipartFile file) throws Exception;
 }
